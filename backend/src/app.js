28d34ee1df6c79253b3d0853e8eb60adceb16528
hungthinh04 <hungthinh2004pt@gmail.com>
2025-07-18 09:46:50 +0700

    config cloud

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const baivietRoutes = require('./routes/baiviet');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
 // const giangvienRoutes = require('./routes/giangvien');
 
@@ -39,6 +41,24 @@ app.use('/lophocphan', require('./routes/lopHocPhan'));
 app.use('/sinhvien_lhp', require('./routes/sinhvien_lhp'));
 app.use('/api/comments', require("./routes/nhanxet"));
 // app.use('/auth', require('./routes/auth'));
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // hoặc 'abc'
+  api_key: process.env.CLOUDINARY_API_KEY, // hoặc '123'
+  api_secret: process.env.CLOUDINARY_API_SECRET // hoặc 'xyz'
+});
 
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads', // Cloudinary folder
+    allowed_formats: ['jpg', 'png', 'jpeg', 'gif']
+  }
+});
+const upload = multer({ storage: storage });
+
+router.post('/uploads', upload.single('file'), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: "No file uploaded" });
+  res.json({ url: req.file.path });
+});
 
 module.exports = app;
